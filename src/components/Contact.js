e50e46d7641a082de6daa2d3e769821096ef0b58
Denis <denisghera@yahoo.com>
2024-08-23 22:09:55 +0300

    some fixes + font stroke added

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import '../App.css';
-import './Contact.css';
+import user from "../images/user-image.jpg"
 
 function Contact() {
   return (
     <div>
         <h1 style={{marginTop:"4vw", textAlign:"center"}}>Contact page</h1>
+        <center><img src={user} alt='user' style={{width:'18vw'}}/></center>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../App.css';
-import './Header.css';
+import logo from "../images/logo512.png"
 
 function Header() {
   return (
     <header className="header">
       <div style={{marginLeft:"5vw"}}>
-        <Link to="/portfolio">
-          <img src="/logo512.png" alt="logo" className="logo" />
+        <Link to="/portfolio" className='logo'>
+          <img src={logo} alt='logo' style={{width:'5vw'}}/>
         </Link>
       </div>
       <nav>
-        <Link to="/about">About</Link>
-        <Link to="/projects">Projects</Link>
-        <Link to="/contact">Contact</Link>
+        <Link to="/about" className='btn'>About</Link>
+        <Link to="/projects" className='btn'>Projects</Link>
+        <Link to="/contact" className='btn'>Contact</Link>
       </nav>
     </header>
   );
